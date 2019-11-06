c9479f0e54a220625c430ff306bec3f59a00da0b
drone <dario_carazo@hotmail.com>
2019-11-06 00:13:37 -0300

    ultimo cambio

--- src/server/routes/Comanda.js
+++ src/server/routes/Comanda.js
@@ -1,15 +1,17 @@
-// const express = require('express');
-// const router=express.Router();
-// const db=require('../modules/database/database');
-// const bodyParser = require('body-parser');
-// router.use(bodyParser.urlencoded({ extended: false }));
-// router.post('/comanda',(req,res)=>{
-//     let body=req.body;
-//     db.query(`CALL AddComanda(${body.IDCliente},${body.Fecha},${body.Total},${body.Numeracion},${body.MovimientosComandas})`,(err,rows,fields)=>{
-//         if(!err){
-//             res.json(rows);
-//         }else{
-//             console.log(err);
-//         }       
-//     })
-// });
+const express = require('express');
+const router=express.Router();
+const db=require('../modules/database/database');
+const bodyParser = require('body-parser');
+var body=bodyParser.json();
+router.post('/comanda',body,(req,res)=>{
+   let body=req.body;
+//   console.log("IDProducto:"+body.MovimientosComandas.IDProducto);
+      db.query(`CALL SP_AddComanda(${body.IDCliente},${body.Fecha},${body.Total},${body.Numeracion},'${JSON.stringify(body.MovimientosComandas)}')`,(err,rows,fields)=>{
+          if(!err){
+              res.json(rows);
+          }else{
+              console.log(err);
+          }       
+      })
+});
+module.exports = router;
